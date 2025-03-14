b697cdd3a47910786262b0d3da7057f8f6638e6d
root <root@DESKTOP-9J3FKR0>
2025-03-14 09:50:38 +0800

    add naive flash mha v2 code

---
+++ ld_triton/ops/attention/naive_flash_mha_v2.py
@@ -0,0 +1,211 @@
+
+import torch
+
+
+class _naive_mha_flash_v1(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, causal, sm_scale, BLOCK_M=128, BLOCK_N=128):
+        assert Q.shape[0] == K.shape[0] == V.shape[0], f'q.shape[0]: {Q.shape[0]}, k.shape[0]: {K.shape[0]}, v.shape[0]: {V.shape[0]}'
+        assert Q.shape[1] == K.shape[1] == V.shape[1], f'q.shape[1]: {Q.shape[1]}, k.shape[1]: {K.shape[1]}, v.shape[1]: {V.shape[1]}'
+        assert Q.shape[2] == K.shape[2] == V.shape[2], f'q.shape[2]: {Q.shape[2]}, k.shape[2]: {K.shape[2]}, v.shape[2]: {V.shape[2]}'
+        assert Q.shape[3] == K.shape[3] == V.shape[3], f'q.shape[3]: {Q.shape[3]}, k.shape[3]: {K.shape[3]}, v.shape[3]: {V.shape[3]}'
+
+        Z, H, N_CTX, HEAD_DIM = Q.shape
+        factory_kwargs = {'device': Q.device, 'dtype': Q.dtype}
+        O = torch.zeros((Z, H, N_CTX, HEAD_DIM), **factory_kwargs)  # [Z, H, N_CTX, HEAD_DIM]
+        L = torch.zeros((Z, H, N_CTX, 1), **factory_kwargs)         # [Z, H, N_CTX, 1]
+        
+        for z in range(Z):
+            for h in range(H):
+                for i in range(0, N_CTX, BLOCK_M):
+                    q = Q[z, h, i: i+BLOCK_M, :]                                  # [BLOCK_M, HEAD_DIM]
+                    o = torch.zeros((BLOCK_M, HEAD_DIM), **factory_kwargs)        # [BLOCK_M, HEAD_DIM]
+                    l = torch.zeros((BLOCK_M, 1), **factory_kwargs)               # [BLOCK_M, 1]
+                    m = torch.full((BLOCK_M, 1), float('-inf'), **factory_kwargs) # [BLOCK_M, 1]
+                    
+                    for j in range(0, N_CTX, BLOCK_N):
+                        k = K[z, h, j: j+BLOCK_N, :] # [BLOCK_N, HEAD_DIM]
+                        v = V[z, h, j: j+BLOCK_N, :] # [BLOCK_N, HEAD_DIM]
+
+                        s = torch.matmul(q, k.t()) * sm_scale                                 # [BLOCK_M, BLOCK_N]
+                        if causal:
+                            offs_m = torch.arange(i, i + BLOCK_M, device=Q.device)[:, None]   # [BLOCK_M, 1]
+                            offs_n = torch.arange(j, j + BLOCK_N, device=Q.device)[None, :]   # [1, BLOCK_N]
+                            s[offs_m < offs_n] = torch.finfo(s.dtype).min                     # [BLOCK_M, BLOCK_N]
+
+                        m_new = torch.max(m, torch.max(s, dim=-1, keepdim=True)[0])           # [BLOCK_M, 1]
+                        p = torch.exp(s.float() - m_new).to(q.dtype)                          # [BLOCK_M, BLOCK_N]
+                        l_new = torch.exp(m - m_new) * l + torch.sum(p, dim=-1, keepdim=True) # [BLOCK_M, 1]
+                        o_new = torch.exp(m - m_new) * o + torch.matmul( p, v)                # [BLOCK_M, HEAD_DIM]
+                        o = o_new # [BLOCK_M, BLOCK_N]
+                        l = l_new # [BLOCK_M, 1]
+                        m = m_new # [BLOCK_M, 1]
+
+                    O[z, h, i: i+BLOCK_M] = (1.0 / l) * o        # [BLOCK_M, HEAD_DIM]
+                    L[z, h, i: i+BLOCK_M, :] = m + torch.log(l)  # [BLOCK_M, 1]
+
+        # for i in range(0, N_CTX, BLOCK_M):
+        #     q = Q[:, :, i: i+BLOCK_M, :]                                        # [Z, H, BLOCK_M, HEAD_DIM]
+        #     o = O[:, :, i: i+BLOCK_M, :]                                        # [Z, H, BLOCK_M, HEAD_DIM]
+        #     l = L[:, :, i: i+BLOCK_M, :]                                        # [Z, H, BLOCK_M, 1]
+        #     m = torch.full((Z, H, BLOCK_M, 1), float('-inf'), **factory_kwargs) # [Z, H, BLOCK_M, 1]
+        #     for j in range(0, N_CTX, BLOCK_N):
+        #         k = K[:, :, j: j+BLOCK_N, :]                          # [Z, H, BLOCK_N, HEAD_DIM]
+        #         v = V[:, :, j: j+BLOCK_N, :]                          # [Z, H, BLOCK_N, HEAD_DIM]
+        #         s = torch.matmul(q, k.permute(0, 1, 3, 2)) * sm_scale # [Z, H, BLOCK_M, BLOCK_N]
+        #         if causal:
+        #             offs_m = torch.arange(i, i + BLOCK_M, device=Q.device)[:, None]   # [BLOCK_M, 1]
+        #             offs_n = torch.arange(j, j + BLOCK_N, device=Q.device)[None, :]   # [1, BLOCK_N]
+        #             s[:, :, offs_m < offs_n] = torch.finfo(s.dtype).min               # [Z, H, BLOCK_M, BLOCK_N]
+
+        #         m_new = torch.max(m, torch.max(s, dim=-1, keepdim=True)[0])            # [Z, H, BLOCK_M, 1]
+        #         p = torch.exp(s.float() - m_new).to(q.dtype)                           # [Z, H, BLOCK_M, BLOCK_N]
+                
+        #         l_new = torch.exp(m - m_new) * l + torch.sum(p, dim=-1, keepdim=True)  # [Z, H, BLOCK_M, 1]
+        #         o_new = torch.exp(m - m_new) * o + torch.matmul( p, v)                 # [Z, H, BLOCK_M, 1]
+        #         o = o_new # [Z, H, BLOCK_M, 1]
+        #         l = l_new # [Z, H, BLOCK_M, 1]
+        #         m = m_new # [Z, H, BLOCK_M, 1]
+
+        #     O[:, :, i: i+BLOCK_M] = (1.0 / l) * o        # [Z, H, BLOCK_M, HEAD_DIM]
+        #     L[:, :, i: i+BLOCK_M, :] = m + torch.log(l)  # [Z, H, BLOCK_M, 1]
+        
+        ctx.save_for_backward(Q, K, V, O, L)
+        ctx.causal = causal
+        ctx.sm_scale = sm_scale
+        ctx.BLOCK_M = BLOCK_M
+        ctx.BLOCK_N = BLOCK_N
+        return O
+    
+    @staticmethod
+    def backward(ctx, dO):
+        Q, K, V, O, L = ctx.saved_tensors
+        Z, H, N_CTX, HEAD_DIM = Q.shape
+        factory_kwargs = {'device': Q.device, 'dtype': Q.dtype}
+        causal = ctx.causal
+        sm_scale = ctx.sm_scale
+        BLOCK_M = ctx.BLOCK_M
+        BLOCK_N = ctx.BLOCK_N
+        dQ = torch.zeros_like(Q)                    # [Z, H, N_CTX, HEAD_DIM]
+        dK = torch.zeros_like(K)                    # [Z, H, N_CTX, HEAD_DIM]
+        dV = torch.zeros_like(V)                    # [Z, H, N_CTX, HEAD_DIM]
+        D = torch.sum(dO * O, dim=-1, keepdim=True) # [Z, H, N_CTX, 1]
+        for z in range(Z):
+            for h in range(H):
+                for j in range(0, N_CTX, BLOCK_N):
+                    k = K[z, h, j: j+BLOCK_N, :]                            # [BLOCK_N, HEAD_DIM]
+                    v = V[z, h, j: j+BLOCK_N, :]                            # [BLOCK_N, HEAD_DIM]
+                    dk = torch.zeros((BLOCK_N, HEAD_DIM), **factory_kwargs) # [BLOCK_N, HEAD_DIM]
+                    dv = torch.zeros((BLOCK_N, HEAD_DIM), **factory_kwargs) # [BLOCK_N, HEAD_DIM]
+                    for i in range(0, N_CTX, BLOCK_M):
+                        q  =  Q[z, h, i: i+BLOCK_M, :] # [BLOCK_M, HEAD_DIM]
+                        o  =  O[z, h, i: i+BLOCK_M, :] # [BLOCK_M, HEAD_DIM]
+                        do = dO[z, h, i: i+BLOCK_M, :] # [BLOCK_M, HEAD_DIM]
+                        dq = dQ[z, h, i: i+BLOCK_M, :] # [BLOCK_M, HEAD_DIM]
+                        l  =  L[z, h, i: i+BLOCK_M, :] # [BLOCK_M, 1]
+                        d  =  D[z, h, i: i+BLOCK_M, :] # [BLOCK_M, 1]
+
+                        s = torch.matmul(q, k.t()) * sm_scale                               # [BLOCK_M, BLOCK_N]
+                        if causal:
+                            offs_m = torch.arange(i, i + BLOCK_M, device=Q.device)[:, None] # [BLOCK_M, 1]
+                            offs_n = torch.arange(j, j + BLOCK_N, device=Q.device)[None, :] # [1, BLOCK_N]
+                            s[offs_m < offs_n] = torch.finfo(s.dtype).min
+                        p = torch.exp(s.float() - l).to(q.dtype)    # [BLOCK_M, BLOCK_N]
+                        dp = torch.matmul(do, v.t())                # [BLOCK_M, BLOCK_N]
+                        d = torch.sum(do * o, dim=-1, keepdim=True) # [BLOCK_M, 1]
+                        ds = p * (dp - d) * sm_scale                # [BLOCK_M, BLOCK_N]
+                        dq = dq + torch.matmul(ds, k)               # [BLOCK_M, HEAD_DIM]
+                        dk = dk + torch.matmul(ds.t(), q)           # [BLOCK_N, HEAD_DIM]
+                        dv = dv + torch.matmul(p.t(), do)           # [BLOCK_N, HEAD_DIM]
+
+                        dQ[z, h, i: i+BLOCK_M, :] = dq              # [BLOCK_M, HEAD_DIM]
+                        
+                    dK[z, h, j: j+BLOCK_N, :] = dk  # [BLOCK_N, HEAD_DIM]
+                    dV[z, h, j: j+BLOCK_N, :] = dv  # [BLOCK_N, HEAD_DIM]
+
+        # for j in range(0, N_CTX, BLOCK_N):
+        #     k = K[:, :, j: j+BLOCK_N, :]                              # [Z, H, BLOCK_N, HEAD_DIM]
+        #     v = V[:, :, j: j+BLOCK_N, :]                              # [Z, H, BLOCK_N, HEAD_DIM]
+        #     dk = torch.zeros((BLOCK_N, HEAD_DIM), **factory_kwargs)   # [Z, H, BLOCK_N, HEAD_DIM]
+        #     dv = torch.zeros((BLOCK_N, HEAD_DIM), **factory_kwargs)   # [Z, H, BLOCK_N, HEAD_DIM]
+        #     for i in range(0, N_CTX, BLOCK_M):
+        #         q  =  Q[:, :, i: i+BLOCK_M, :]  # [Z, H, BLOCK_M, HEAD_DIM]
+        #         o  =  O[:, :, i: i+BLOCK_M, :]  # [Z, H, BLOCK_M, HEAD_DIM]
+        #         do = dO[:, :, i: i+BLOCK_M, :]  # [Z, H, BLOCK_M, HEAD_DIM]
+        #         dq = dQ[:, :, i: i+BLOCK_M, :]  # [Z, H, BLOCK_M, HEAD_DIM]
+        #         l  =  L[:, :, i: i+BLOCK_M, :]  # [Z, H, BLOCK_M, 1]
+        #         d  =  D[:, :, i: i+BLOCK_M, :]  # [Z, H, BLOCK_M, 1]
+        #         s = torch.matmul(q, k.permute(0, 1, 3, 2)) * sm_scale               # [Z, H, BLOCK_M, BLOCK_N]
+        #         if causal:
+        #             offs_m = torch.arange(i, i + BLOCK_M, device=Q.device)[:, None] # [BLOCK_M, 1]
+        #             offs_n = torch.arange(j, j + BLOCK_N, device=Q.device)[None, :] # [1, BLOCK_N]
+        #             s[:, :, offs_m < offs_n] = torch.finfo(s.dtype).min             # [Z, H, BLOCK_M, BLOCK_N]
+        #         p = torch.exp(s.float() - l).to(q.dtype)           # [Z, H, BLOCK_M, BLOCK_N]
+        #         dp = torch.matmul(do, v.permute(0, 1, 3, 2))       # [Z, H, BLOCK_M, BLOCK_N]
+        #         d = torch.sum(do * o, dim=-1, keepdim=True)        # [Z, H, BLOCK_M, 1]
+        #         ds = p * (dp - d) * sm_scale                       # [Z, H, BLOCK_M, BLOCK_N]
+        #         dq = dq + torch.matmul(ds, k)                      # [Z, H, BLOCK_M, HEAD_DIM]
+        #         dk = dk + torch.matmul(ds.permute(0, 1, 3, 2), q)  # [Z, H, BLOCK_N, HEAD_DIM]
+        #         dv = dv + torch.matmul(p.permute(0, 1, 3, 2), do)  # [Z, H, BLOCK_N, HEAD_DIM]
+
+        #         dQ[:, :, i: i+BLOCK_M, :] = dq                     # [Z, H, BLOCK_M, HEAD_DIM]
+        #     dK[:, :, j: j+BLOCK_N, :] = dk  # [Z, H, BLOCK_N, HEAD_DIM]
+        #     dV[:, :, j: j+BLOCK_N, :] = dv  # [Z, H, BLOCK_N, HEAD_DIM]
+
+        return dQ, dK, dV, None, None, None, None
+
+
+naive_mha_flash_v1 = _naive_mha_flash_v1.apply
+
+
+if __name__ == '__main__':
+    # Z = 1
+    # H = 1
+    # N_CTX = 4
+    # HEAD_DIM = 8
+    # BLOCK_M = 2
+    # BLOCK_N = 2
+
+    Z = 2
+    H = 3
+    N_CTX = 1024
+    HEAD_DIM = 32
+    BLOCK_M = 128
+    BLOCK_N = 128
+
+    dtype_ = [torch.float32, torch.float16]
+    dtype_ = [torch.float32]
+    for dtype in dtype_:
+        q = (torch.empty((Z, H, N_CTX, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5).requires_grad_())
+        k = (torch.empty((Z, H, N_CTX, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5).requires_grad_())
+        v = (torch.empty((Z, H, N_CTX, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5).requires_grad_())
+        dout = torch.randn_like(q)
+        causal_ = [False, True]
+        # causal_ = [False]
+        sm_scale = 0.5
+        for causal in causal_:
+            p: torch.Tensor = torch.matmul(q, k.transpose(2, 3)) * sm_scale
+            if causal:
+                M = torch.tril(torch.ones((N_CTX, N_CTX), device=q.device))
+                p[:, :, M == 0] = float('-inf')
+            p = torch.softmax(p.float(), dim=-1).to(dtype)
+            o = torch.matmul(p, v)
+            o.backward(dout)
+            dq, q.grad = q.grad.clone(), None
+            dk, k.grad = k.grad.clone(), None
+            dv, v.grad = v.grad.clone(), None
+
+            naive_o = naive_mha_flash_v1(q, k, v, causal, sm_scale, BLOCK_M, BLOCK_N)
+            naive_o.backward(dout)
+            naive_dq, q.grad = q.grad.clone(), None
+            naive_dk, k.grad = k.grad.clone(), None
+            naive_dv, v.grad = v.grad.clone(), None
+
+            atol = 1e-2
+            rtol = 1e-2
+            assert torch.allclose(o, naive_o, atol=atol, rtol=rtol), f'Z: {Z}, H: {H}, N_CTX: {N_CTX}, HEAD_DIM: {HEAD_DIM}, causal: {causal}, dtype: {dtype}'
+            assert torch.allclose(dq, naive_dq, atol=atol, rtol=rtol), f'Z: {Z}, H: {H}, N_CTX: {N_CTX}, HEAD_DIM: {HEAD_DIM}, causal: {causal}, dtype: {dtype}'
+            assert torch.allclose(dk, naive_dk, atol=atol, rtol=rtol), f'Z: {Z}, H: {H}, N_CTX: {N_CTX}, HEAD_DIM: {HEAD_DIM}, causal: {causal}, dtype: {dtype}'
+            assert torch.allclose(dv, naive_dv, atol=atol, rtol=rtol), f'Z: {Z}, H: {H}, N_CTX: {N_CTX}, HEAD_DIM: {HEAD_DIM}, causal: {causal}, dtype: {dtype}'
+            
+            
